dbfb24df7875c86681bcc6a115849a971b93040b
gokhan-kaplan <gokhan.kaplan519@outlook.com>
2023-11-09 16:15:59 +0300

    S7G1

--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function KaydedilenlerListesi(props) {
+  const history = useHistory();
+  function handleClick() {
+    history.push("/");
+  }
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
       {props.list.map((movie) => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <button className="home-button">Anasayfa</button>
+      <div className="home-button" onClick={handleClick}>
+        Anasayfa
+      </div>
     </div>
   );
 }
